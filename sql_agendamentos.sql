42f28d378039e22a94b365ebc25a11880777adac
luizcarlos.fonseca <luizcarlos.fonseca@codesp1855.APS.TI>
2016-04-18 17:56:47 -0300

    configurando o banco para o plugin "burzum/file-storage": "1.1.*"

---
+++ sql_agendamentos.sql
@@ -0,0 +1,12 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  luizcarlos.fonseca
+ * Created: 18/04/2016
+ */
+
+-- Criar a base de dados 
+create database db_agendamentos default character set utf8 collate utf8_general_ci;
